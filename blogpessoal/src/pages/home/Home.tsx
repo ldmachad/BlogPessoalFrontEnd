06c03ff77b718d034a38a83ca1a514856ac39015
ldmachad <leonardodiomachado@gmail.com>
2023-10-25 10:10:51 -0300

    Update Blogpessoal

--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,38 +1,36 @@
-import './Home.css'
 
 function Home() {
   return (
-    <>
-      <header>
-        <div className="header">
-        <h1>BLOG PESSOAL</h1> 
-        <ul>
-          <li><a href="#">Postagens</a></li>
-          <li><a href="#">Temas</a></li>
-          <li><a href="#">Cadastrar tema</a></li>
-          <li><a href="#">Perfil</a></li>
-          <li><a href="#">Sair</a></li>
-        </ul>
-        </div>
+      <>
+          <div className="bg-indigo-900 flex justify-center">
+              <div className='container grid grid-cols-2 text-white'>
+                  <div className="flex flex-col gap-4 items-center justify-center py-4">
+                      <h2 className='text-5xl font-bold'>
+                          Seja Bem Vinde!
+                      </h2>
+                      <p className='text-xl'>
+                          Expresse aqui seus pensamentos e opniões
+                      </p>
 
-        <div className="container">
+                      <div className="flex justify-around gap-4">
+                          <div className='rounded text-white 
+                                          border-white border-solid border-2 py-2 px-4'
+                              >
+                              Nova Postagem
+                          </div>
+                      </div>
+                  </div>
 
-          <div className="txt-buttons">
-
-        <div className="txt-container">
-          <h1>Seja bem vinde!</h1>
-          <p>Expresse aqui seus pensamentos e opiniões</p></div>
-        <div className="buttons">
-          <button className='novapostagem'>Nova postagem</button>
-          <button className="verpostagem">Ver postagens</button>
-        </div>
-
-        </div>
-        
-        <div className="img"><img src="https://i.imgur.com/VpwApCU.png" alt=""/></div>
-        </div>
-      </header>             
-    </>
+                  <div className="flex justify-center ">
+                      <img
+                          src="https://i.imgur.com/fyfri1v.png"
+                          alt="Imagem Página Home"
+                          className='w-2/3'
+                      />
+                  </div>
+              </div>
+          </div>
+      </>
   )
 }
 
